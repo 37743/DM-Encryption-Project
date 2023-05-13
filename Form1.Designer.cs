d1e8458381e4dba37ba757b3e5fbcb4164fdd0d1
37743 <yousef.ibrahim.gomaa@gmail.com>
2023-05-13 21:00:55 +0300

    Created a simple interface along with the cryptography logic.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DM_Encryption_Project
 {
-    partial class Form1
+    partial class window
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            encryptBox = new TextBox();
+            groupBox1 = new GroupBox();
+            decryptText = new Label();
+            encryptText = new Label();
+            decryptBox = new TextBox();
+            decrypt = new Button();
+            encrypt = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // encryptBox
+            // 
+            encryptBox.Location = new Point(6, 35);
+            encryptBox.Name = "encryptBox";
+            encryptBox.Size = new Size(248, 23);
+            encryptBox.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(decryptText);
+            groupBox1.Controls.Add(encryptText);
+            groupBox1.Controls.Add(decryptBox);
+            groupBox1.Controls.Add(decrypt);
+            groupBox1.Controls.Add(encrypt);
+            groupBox1.Controls.Add(encryptBox);
+            groupBox1.Location = new Point(12, 3);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(260, 146);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            // 
+            // decryptText
+            // 
+            decryptText.AutoSize = true;
+            decryptText.Location = new Point(6, 67);
+            decryptText.Name = "decryptText";
+            decryptText.Size = new Size(152, 15);
+            decryptText.TabIndex = 5;
+            decryptText.Text = "Enter text below to decrypt:";
+            // 
+            // encryptText
+            // 
+            encryptText.AutoSize = true;
+            encryptText.Location = new Point(6, 17);
+            encryptText.Name = "encryptText";
+            encryptText.Size = new Size(152, 15);
+            encryptText.TabIndex = 4;
+            encryptText.Text = "Enter text below to encrypt:";
+            // 
+            // decryptBox
+            // 
+            decryptBox.Location = new Point(6, 85);
+            decryptBox.Name = "decryptBox";
+            decryptBox.Size = new Size(248, 23);
+            decryptBox.TabIndex = 3;
+            // 
+            // decrypt
+            // 
+            decrypt.Location = new Point(154, 117);
+            decrypt.Name = "decrypt";
+            decrypt.Size = new Size(100, 23);
+            decrypt.TabIndex = 2;
+            decrypt.Text = "Decrypt";
+            decrypt.UseVisualStyleBackColor = true;
+            decrypt.Click += decrypt_Click;
+            // 
+            // encrypt
+            // 
+            encrypt.Location = new Point(6, 117);
+            encrypt.Name = "encrypt";
+            encrypt.Size = new Size(100, 23);
+            encrypt.TabIndex = 1;
+            encrypt.Text = "Encrypt";
+            encrypt.UseVisualStyleBackColor = true;
+            encrypt.Click += encrypt_Click;
+            // 
+            // window
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(284, 161);
+            Controls.Add(groupBox1);
+            Name = "window";
+            Text = "Encryption Project - 207";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TextBox encryptBox;
+        private GroupBox groupBox1;
+        private TextBox decryptBox;
+        private Button decrypt;
+        private Button encrypt;
+        private Label encryptText;
+        private Label decryptText;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,54 @@
+using System.Windows.Forms;
+
 namespace DM_Encryption_Project
 {
-    public partial class Form1 : Form
+    public partial class window : Form
     {
-        public Form1()
+        private string encryption(string msg, int key)
+        {
+            char[] encryptMsg = new char[msg.Length];
+            Console.Clear();
+            for (int i = 0; i < msg.Length; i++)
+            {
+                char chardiff = ' ';
+                Console.WriteLine((int)msg[i]);
+                if (msg[i] >= 65 && msg[i] <= 90) encryptMsg[i] = (char)((msg[i] + key) % 26 + 'A');
+                else if (msg[i] >= 97 && msg[i] <= 122) encryptMsg[i] = (char)((msg[i] - 32 + key) % 26 + 'a');
+                Console.WriteLine(chardiff);
+            }
+            string returnMsg = new(encryptMsg);
+            return returnMsg;
+        }
+        private string decryption(string msg, int key)
+        {
+            char[] decryptMsg = new char[msg.Length];
+            Console.Clear();
+            for (int i = 0; i < msg.Length; i++)
+            {
+                Console.WriteLine((int)msg[i]);
+                if (msg[i] >= 65 && msg[i] <= 90) decryptMsg[i] = (char)((msg[i] - key) % 26 + 'A');
+                else if (msg[i] >= 97 && msg[i] <= 122) decryptMsg[i] = (char)((msg[i] - 32 - key) % 26 + 'a');
+            }
+            string returnMsg = new(decryptMsg);
+            return returnMsg;
+        }
+        public window()
         {
             InitializeComponent();
         }
+
+        private void decrypt_Click(object sender, EventArgs e)
+        {
+            encryptBox.Text = decryption(decryptBox.Text, GlobalVars.key);
+        }
+
+        private void encrypt_Click(object sender, EventArgs e)
+        {
+            decryptBox.Text = encryption(encryptBox.Text, GlobalVars.key);
+        }
+    }
+    public static class GlobalVars
+    {
+        public const int key = 43;
     }
 }
